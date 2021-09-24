3d98abd13e060d36627ecf4c66e934923e1fb27f
Shanelka Pramuditha <74038575+ShanelkaPramuditha@users.noreply.github.com>
2021-09-24 10:25:36 +0530

    Update bot.py
    Changed full program and added age and lived days calculater....

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 from pyrogram import Client, filters
-from pyrogram.types import MessageEntity
-import tgcrypto 
+import datetime
+import pytz
+
+#get time now as time_now
+
+time_zone = pytz.timezone('Asia/Colombo')
+today = datetime.datetime.now(tz = time_zone)
 
 #get telegram account access
 
@@ -10,12 +15,16 @@ bot = Client("NICSriLankaBot", config_file = "config.ini")
 
 @bot.on_message(filters.command(commands = ["start"]))
 def welcome(client, message):
+    bot.set_parse_mode()
     message.reply_text(text = """Hi Welcome...
 I'm @NICSriLankaBOT
-I can calculate birthday with new and old NIC numbers.
+I can calculate birthday and age with new and old NIC numbers.
 
-Enter NIC Number...""")
+@NICSriLankaBOT Github repo: <a href="https://github.com/shanelkapramuditha/NIC-Sri-Lanka-BOT">here</a>
 
+Enter NIC Number...""", disable_web_page_preview=True)
+
+###########calculation part
 #get correct id
 
 @bot.on_message(filters.text)
@@ -23,126 +32,117 @@ def start(client, message):
     def check_id():
         while True:
             id_number = (message.text).upper()
+
+            #check new nic
             if len(id_number) == 12:
-                if (0 < int(id_number[4:7]) < 366 or 0 < int(id_number[4:7]) - 500 < 366) and 1900 < int(id_number[0:4]) < 2004:
-                    return id_number, int(id_number[0:4]), int(id_number[4:7])
-                else:
-                    message.reply_text("Please check your NIC number and Enter again...")
-                    return "None"
-            elif len(id_number) == 10:
-                if "V" in id_number[9]:
-                    if 0 < int(id_number[2:5]) < 366 or 0 < int(id_number[2:5]) - 500 < 366:
-                        return id_number, int("19" + id_number[0:2]), int(id_number[2:5])
+                birth_year = int(id_number[0:4])
+                birth_days = int(id_number[4:7])
+                if (datetime.date(birth_year + 1, 1, 1) - datetime.date(birth_year, 1,1) == datetime.timedelta(days=366)):
+                    if (0 < birth_days <= 366 or 0 < birth_days - 500 <= 366) and today.year - birth_year >= 16 and birth_year > 1900:
+                        return id_number, birth_year, birth_days, "Leap year"
                     else:
                         message.reply_text("Please check your NIC number and Enter again...")
-                        return "None"
-                else:
-                    message.reply_text("Please check your NIC number and Enter again...")
-                    return "None"
+                        return "wrong"
+                elif datetime.date(birth_year + 1, 1, 1) - datetime.date(birth_year, 1,1) == datetime.timedelta(days=365):
+                    if (0 < birth_days <= 365 or 0 < birth_days - 500 <= 365) and today.year - birth_year >= 16 and birth_year > 1900:
+                        return id_number, birth_year, birth_days, "Not Leap year"
+                    else:
+                        message.reply_text("Please check your NIC number and Enter again...")
+                        return "wrong"
+
+            #check old nic
+            elif len(id_number) == 10:
+                birth_year = int("19"+str(id_number[0:2]))
+                birth_days = int(id_number[2:5])
+                
+                if "V" in id_number[9] and datetime.date(birth_year + 1, 1, 1) - datetime.date(birth_year, 1,1) == datetime.timedelta(days=366):
+                    if (0 < birth_days <= 366 or 0 < birth_days - 500 <= 366) and today.year - birth_year >= 16 and birth_year > 1900:
+                        return id_number, birth_year, birth_days, "Leap year"
+                    else:
+                        message.reply_text("Please check your NIC number and Enter again...")
+                        return "wrong"
+                elif "V" in id_number[9] and (datetime.date(birth_year + 1, 1, 1) - datetime.date(birth_year, 1,1) == datetime.timedelta(days=365)):
+                    if (0 < birth_days <= 365 or 0 < birth_days - 500 <= 365) and today.year - birth_year >= 16 and birth_year > 1900:
+                        return id_number, birth_year, birth_days, "Not Leap year"
+                    else:
+                        message.reply_text("Please check your NIC number and Enter again...")
+                        return "wrong"
+            
+            #wrong nic
             else:
                 message.reply_text("Please check your NIC number and Enter again...")
-                return "None"
+                return "wrong"
 
-    id_year_days = check_id()
-    id_number = id_year_days[0]
+    id_info = check_id()
 
-    if id_year_days != "None":
-        year = id_year_days[1]
-        days = id_year_days[2]
+    if id_info != "wrong":
+        id_number = id_info[0]
+        birth_year = id_info[1]
+        birth_days = id_info[2]
+        year_type = id_info[3]
+    
+        #check gender and count days
 
-        #check gender
-
-        if days < 500:
+        if birth_days < 500:
             gender = "Male"
         else:
             gender = "Female"
+            birth_days = birth_days - 500
 
-        #count total days
+        #calculate birth day
 
-        if days >= 500:
-            days = days - 500
-        else:
-            days = days
+        birth_day = datetime.timedelta(birth_days - 1) + datetime.datetime(birth_year, 1, 1)
 
-        #Check month and day (Leap year format)
+        #skip feb 29
 
-        if days <= 31:
-            month = "January"
-            day = days - 0
-        elif days <= 60:
-            month = "February"
-            day = days - 31
-        elif days <= 91:
-            month = "March"
-            day = days - 60
-        elif days <= 121:
-            month = "April"
-            day = days - 91
-        elif days <= 152:
-            month = "May"
-            day = days - 121
-        elif days <= 182:
-            month = "June"
-            day = days - 152
-        elif days <= 213:
-            month = "July"
-            day = days - 182
-        elif days <= 244:
-            month = "Auguest"
-            day = days - 213
-        elif days <= 274:
-            month = "September"
-            day = days - 244
-        elif days <= 305:
-            month = "Octomber"
-            day = days - 274
-        elif days <= 335:
-            month = "November"
-            day = days - 305
-        elif days <= 366:
-            month = "December"
-            day = days - 335
-        else:
-            message.reply_text("Something Wrong! Please Check Your Identy Card Number...")
+        if year_type == "Not Leap year" and birth_day.month > 2:
+            birth_day = datetime.timedelta(birth_days - 2) + datetime.datetime(birth_year, 1, 1)
 
-        #check year (leap year or not)
+        #calculate age in days
 
-        def check_year():
-            if year % 4 == 0:
-                if year % 100 == 0:
-                    if year % 400 == 0:
-                        return "Leap year"
-                    else:
-                        return "Not Leap year"
-                else:
-                    return "Leap year"
+        today_str = str(today)[:10].split('-')
+        lived_days = datetime.datetime(int(today_str[0]), int(today_str[1]), int(today_str[2])) - birth_day
+        lived_days = str(lived_days).split(', ')[0]
+
+        # age in year, month, days
+        if today.month >= birth_day.month:
+            age_year = str(today.year - birth_day.year) + " years"
+            if today.day >= birth_day.day:
+                age_month = str(today.month - birth_day.month) + " months"
+                age_days = str(today.day - birth_day.day) + " days"
             else:
-                return "Not Leap year"
-        year_type = check_year()
+                age_month = str(today.month - birth_day.month - 1) + " months"
+                age_month_int = today.month - birth_day.month - 1
+                age_days = str(datetime.datetime(today.year, today.month, today.day) - datetime.datetime(today.year, birth_day.month + age_month_int, birth_day.day)).split(", ")[0]
 
-        #change "days" data type to str
-
-        if day < 10:
-            day = "0" + str(day)
         else:
-            day = day
+            age_year = str(today.year - birth_day.year - 1) + " years"
+
+        age = f"{age_year} {age_month} {age_days}"
 
         #message.reply_text output
 
         bot.set_parse_mode()
         message.reply_text(f"""
-        NIC Number : {id_number}
-Birth Day  : {str(year)} {month} {day}
-Gender     : {gender}
+        ```NIC Number : {id_number}
+
+Birth Day : {birth_day.strftime('%Y %B %d')}
+
+Gender : {gender}
+
+Age : {age}
+
+Number of days lived : {lived_days}
 
 Birth Year Type : {year_type}
-    
-**Now, You can enter other NIC number...**
+
+
+Checked Date : {today.strftime('%Y/%m/%d')}```
+
 
 @NICSriLankaBOT Github repo: <a href="https://github.com/shanelkapramuditha/NIC-Sri-Lanka-BOT">here</a>
+
+**Now, You can enter other NIC number...**
 """, disable_web_page_preview=True)
         
-
-
-
 bot.run()
